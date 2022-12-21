0da02a3664d7c5f40ba3c246968de896a00b70d6
Sidga <120934728+Sidga@users.noreply.github.com>
2022-12-21 19:27:55 +0530

    Add files via upload

---
+++ siddharth.py
@@ -0,0 +1,10 @@
+# Assignment 2 , 1st question
+
+str1 = ("Python is a case sensitive language")
+print(len(str1))
+print(str1[::-1])
+str2 = print(str1[10:35:1])
+str2 = ("object oriented")
+print(str2)
+print(str2.replace(" ",""))
+print(str1.index('a'))
---
+++ siddharth2.py
@@ -0,0 +1,9 @@
+# Assignment 2 ,2nd question
+Name = input("Enter your name")
+SID = int(input("Enter your SID"))
+Department_name = input("Enter your department ")
+CGPA = float(input("Enter your CGPA"))
+print("Hey,",Name,"Here!")
+print("My SID is",SID)
+print("I am from",Department_name,"department and my CGPA is ",CGPA)
+
