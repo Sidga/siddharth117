6ac0560bce86bd0b5097d66ab54375e7dd78c930
Sidga <120934728+Sidga@users.noreply.github.com>
2022-12-22 15:55:46 +0530

    Update siddharth2.py

--- siddharth2.py
+++ siddharth2.py
@@ -1,8 +1,8 @@
 # Assignment 2 ,2nd question
-Name = input("Enter your name")
-SID = int(input("Enter your SID"))
-Department_name = input("Enter your department ")
-CGPA = float(input("Enter your CGPA"))
+Name = ("Siddharth")
+SID = ("22105117")
+Department_name = ("Electronics and communication engineering  ")
+CGPA = ("9.9")
 print("Hey,",Name,"Here!")
 print("My SID is",SID)
 print("I am from",Department_name,"department and my CGPA is ",CGPA)
