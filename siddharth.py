27b910455f3aa640fccb86c942445565d7c3ceb7
Sidga <120934728+Sidga@users.noreply.github.com>
2022-12-22 16:01:59 +0530

    Update siddharth.py

---
+++ siddharth.py
@@ -0,0 +1,7 @@
+# Python Program to find the average of three numbers
+
+a = int(input("Enter 1st number"))
+b = int(input("Enter 2nd number"))
+c = int(input("Enter 3rd number"))
+average = (a+b+c)/3
+print("The average of three numbers is",average)
