edd13984b6f11e1c870e5dbfe187b2730c71ad13
Sidga <120934728+Sidga@users.noreply.github.com>
2023-01-10 10:11:32 +0530

    Add files via upload

---
+++ assnt2,q1.py
@@ -0,0 +1,8 @@
+str = ("Python is a case sensitive language")
+print(len(str))
+print(str[::-1])
+str1 = str[10:26]
+print(str1)
+print(str.replace("Python is a case sensitive language","object oriented"))
+print(str.index("a"))
+print(str.replace(" ",""))
