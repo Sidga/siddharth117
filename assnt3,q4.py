50d9fec97c968c6356f7b47c0ed42fa6eef61e8b
Sidga <120934728+Sidga@users.noreply.github.com>
2023-01-10 10:24:49 +0530

    Add files via upload

---
+++ assnt3,q4.py
@@ -0,0 +1,19 @@
+a = int(input("Enter your grade points"))
+if a == 4 :
+    print("Your grade is D and Poor performance")
+elif a == 5:
+    print("Your grade is C and Below average ")
+elif a == 6:
+    print("Your grade is C+ and Average performance")
+elif a == 7:
+    print("Your grade is B and Good performance")
+elif a == 8:
+    print("Your grade is B+ and Very Good performance")
+elif a == 9:
+    print("Your grade is A and Excellent performance")
+elif a == 10:
+    print("Your grade is A+ and Outstanding performance")
+elif a<4:
+    print("Please enter a valid grade points")
+else:
+    print("Please enter a valid grade points")
